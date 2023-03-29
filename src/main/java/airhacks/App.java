f9d74b3ef12caea41909322981fcc582e08c0130
Jashu98 <127478916+Jashu98@users.noreply.github.com>
2023-03-29 19:32:20 +0530

    save changes

--- src/main/java/airhacks/App.java
+++ src/main/java/airhacks/App.java
@@ -1,13 +1,3 @@
-package airhacks;
-
-
-/**
- *
- * @author airhacks.com
- */
-interface App {
-
-    static void main(String... args) {
-        System.out.println("is working");
-    }
-}
+<h1>thankyou<h1>
+<h2>johnny<h2>
+<h3>i love you<h3>
